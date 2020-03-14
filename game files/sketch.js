65b4b504c8625b76dc30464a0c5376dd6cc73104
Aniket Raj <aniketronaldo10@gmail.com>
2020-03-14 23:19:10 +0530

    Added some Pads

--- game files/sketch.js
+++ game files/sketch.js
@@ -1,9 +1,11 @@
-var width, height, ball;
+var width, height, ball, pad1, pad2;
 function setup() 
 {
     width=600;
     height=600;
     ball = new Ball();
+    pad1 = new Pad(20, height / 2);
+    pad2 = new Pad(width - 20, height / 2);
     createCanvas(width, height);
 }
 
@@ -11,6 +13,8 @@ function draw()
 {
     background(0);
     ball.show();
+    pad1.show();
+    pad2.show();
 }
 
 class Ball 
@@ -30,6 +34,23 @@ class Ball
         ellipse(this.x, this.y, 20, 20);
     }
 }
+
+    class Pad
+    {
+        constructor(x ,y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+
+        show()
+        {
+            rectMode(CENTER);
+            fill(255);
+            rect(this.x ,this.y , 10 ,100);
+        }
+    }
+
 
 
 
